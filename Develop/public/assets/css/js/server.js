0c1cd855f9b93dbd105233a8ed3c23af6691df52
Victoria Drier <toridrier@yahoo.com>
2023-08-10 18:14:18 -0500

    created function to add new note

--- Develop/public/assets/css/js/server.js
+++ Develop/public/assets/css/js/server.js
@@ -26,3 +26,40 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+// serve note page 
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+// catch all route to handle unknown paths 
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// Function to create a new note 
+
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray)) {
+        notesArray = [];
+    }
+    
+    if (notesArray.length === 0) {
+        notesArray.push(0);
+    }
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+
+//route to create new note 
+
